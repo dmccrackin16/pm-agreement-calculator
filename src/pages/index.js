f5a2e4455933818540e81919c90ba6eb5f492d60
dmccrackin16 <dale.mccrackin@yahoo.com>
2025-02-11 08:06:48 -0600

    Add main page component

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import PMCalculator from '../components/PMCalculator';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8 px-4">
+      <div className="max-w-4xl mx-auto">
+        <PMCalculator />
+      </div>
+    </div>
+  );
+}
